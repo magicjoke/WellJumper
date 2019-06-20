1ef6e8aa8ecc3ea9ef4feaa95fe28b84b95e3875
Igor <magicjoke@gmail.com>
2019-06-20 18:16:54 +0300

    20.06 #0

---
+++ WellJumper/Assets/Scripts/BlockSpawnController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawnController : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+
+                Instantiate(prefab, hit.collider.gameObject.transform.position, Quaternion.identity);
+
+            }
+        }
+    }
+
+}
